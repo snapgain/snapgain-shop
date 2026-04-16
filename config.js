7dfab52d0e9f7e8d9ea85b59aa5304396563bd31
Denys Melo <denysgmelo@gmail.com>
2026-04-16 13:49:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,8 @@
 // These are set as Environment Variables in Vercel
 // ─────────────────────────────────────────────
 const SUPABASE_URL  = window.ENV_SUPABASE_URL  || 'https://auhtwkvwbgvekvwctcaj.supabase.co';
-const SUPABASE_ANON = window.ENV_SUPABASE_ANON || 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_ANON = window.ENV_SUPABASE_ANON || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1aHR3a3Z3Ymd2ZWt2d2N0Y2FqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNDY0MjIsImV4cCI6MjA3MjkyMjQyMn0.uJifWx65COLPz5xwI_6bXrHgH7QDaSDiKn7ocsNhg30
+';
 const PRODUCT_SLUG  = 'from-cashback-to-flights';
 const PRODUCT_ID    = 'd88f9512-d0b9-4fb5-9773-72ff4dd86d3b';
-const EBOOK_PRICE   = '£30';
+const EBOOK_PRICE   = '£49.97';
